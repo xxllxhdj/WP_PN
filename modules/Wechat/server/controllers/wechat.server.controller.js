9fe1591a1321bca197f181dc18eafe8d64492d8c
xuxle <xuxle@yonyou.com>
2017-01-23 14:32:03 +0800

    图文消息

--- modules/Wechat/server/controllers/wechat.server.controller.js
+++ modules/Wechat/server/controllers/wechat.server.controller.js
@@ -8,5 +8,14 @@ exports.wechatEvent = function(message, req, res, next) {
         res.reply('文本消息');
         return;
     }
+    if (message.EventKey === 'MSG_PIC') {
+        res.reply([{
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }]);
+        return;
+    }
     next();
 };
