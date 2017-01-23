51700be228603da3cc4aadef946b2eb78bd5d7a3
xxl <tx082xuxl@126.com>
2017-01-23 20:57:20 +0800

    图文消息

--- modules/Wechat/server/controllers/wechat.server.controller.js
+++ modules/Wechat/server/controllers/wechat.server.controller.js
@@ -4,12 +4,45 @@ var path = require('path'),
     config = require(path.resolve('./config/config'));
 
 exports.wechatEvent = function(message, req, res, next) {
+    if (message.Event === 'subscribe') {
+        res.reply('您好！感谢关注xxllxhdj的接口测试号');
+        return;
+    }
     if (message.EventKey === 'MSG_TEXT') {
         res.reply('文本消息');
         return;
     }
     if (message.EventKey === 'MSG_PIC') {
         res.reply([{
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }]);
+        return;
+    }
+    if (message.EventKey === 'MSG_PICS') {
+        res.reply([{
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }, {
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }, {
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }, {
+            title: '微信UI',
+            description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
+            picurl: 'http://101.251.244.131:6500/img/weui.png',
+            url: 'https://weui.io/'
+        }, {
             title: '微信UI',
             description: 'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。',
             picurl: 'http://101.251.244.131:6500/img/weui.png',
